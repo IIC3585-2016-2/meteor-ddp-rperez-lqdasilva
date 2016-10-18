44240b4ec979200147b896fbfc8b4f9d62410963
Luiz Quelves da Silva <luiz.quelves@gmail.com>
2016-10-18 18:10:09 -0300

    Cliente side, meteor app like whatsapp

---
+++ whatsapp/client/scripts/filters/calendar.filter.js
@@ -0,0 +1,20 @@
+/**
+ * Created by quelves on 10/18/16.
+ */
+import Moment from 'moment';
+import { Filter } from 'angular-ecmascript/module-helpers';
+
+export default class CalendarFilter extends Filter {
+    filter(time) {
+        if (!time) return;
+
+        return Moment(time).calendar(null, {
+            lastDay : '[Yesterday]',
+            sameDay : 'LT',
+            lastWeek : 'dddd',
+            sameElse : 'DD/MM/YY'
+        });
+    }
+}
+
+CalendarFilter.$name = 'calendar';
